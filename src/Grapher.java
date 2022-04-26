2cf7eeef071a12e11430bb55f6d01eef5ed6430b
xiao278 <90475159+xiao278@users.noreply.github.com>
2022-04-26 15:16:44 -0400

    implemented marching square
    flawed,  seems to not be smooth when going in the "ccw" direction. algorithm needs to be more thorough. Also implement high res

--- src/Grapher.java
+++ src/Grapher.java
@@ -7,6 +7,8 @@ import java.awt.event.*;
 
 public class Grapher {
     private static Expression expression;
+    private static double threshold;
+    private static boolean implicit;
     private static JFrame frame;
     private static JPanel canvas;
     private static Color MARKER = new Color(232,232,232);
@@ -15,6 +17,7 @@ public class Grapher {
     private static Point lastMousePos;
     private static double SCALE = 0.01;
     private static int samples = 250;
+    private static final int sqSize = 10;
 
     private static final int LEFT = 0;
     private static final int RIGHT = 1;
@@ -26,7 +29,7 @@ public class Grapher {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000,600);
         JPanel gui = new JPanel();
-        JTextField input = new JTextField(18);
+        JTextField input = new JTextField("x^2", 20);
         JButton inputButton = new JButton("Graph it");
         inputButton.addActionListener(new ActionListener() {
             @Override
@@ -37,13 +40,22 @@ public class Grapher {
                     return;
                 }
                 try {
-                    expression = new ExpressionBuilder(raw).variable("x").build();
-                } catch (IllegalArgumentException e) {
+                    String[] split = new String[2];
+                    if (raw.indexOf('=') != -1) {
+                        split = raw.split("=");
+                        expression = new ExpressionBuilder(split[0]).variable("x").variable("y").build();
+                        threshold = Double.parseDouble(split[1]);
+                        implicit = true;
+                    }
+                    else {
+                        expression = new ExpressionBuilder(raw).variable("x").build();
+                        implicit = false;
+                    }
+                } catch (Exception e) {
                     showError(e.getMessage());
                     return;
                 }
-                //temporary
-                graph();
+                regraph();
             }
         });
         gui.add(input);
@@ -54,6 +66,7 @@ public class Grapher {
             public void mousePressed(MouseEvent e) {
                 super.mousePressed(e);
                 lastMousePos = e.getPoint();
+                //showError("" + pixToCart(e.getX(), e.getY())[0]);
             }
         });
         frame.addMouseMotionListener(new MouseMotionAdapter() {
@@ -63,14 +76,14 @@ public class Grapher {
                 offsetX += (lastMousePos.x - e.getX()) / (double) frame.getWidth() * 2 * frame.getWidth() * SCALE;
                 offsetY += (e.getY() - lastMousePos.y) / (double) frame.getHeight() * 2 * frame.getHeight() * SCALE;
                 lastMousePos = e.getPoint();
-                graph();
+                regraph();
             }
         });
         frame.addMouseWheelListener(new MouseWheelListener() {
             @Override
             public void mouseWheelMoved(MouseWheelEvent e) {
                 SCALE *= Math.pow(1.1,e.getWheelRotation());
-                graph();
+                regraph();
             }
         });
         canvas = new JPanel();
@@ -82,7 +95,17 @@ public class Grapher {
         JOptionPane.showMessageDialog(null,message,"Error",JOptionPane.ERROR_MESSAGE);
     }
     private static void implicitGraph(){
-        showError("not ready");
+        /*dimension [x][y]*/
+        int xn = frame.getWidth()/sqSize;
+        int yn = frame.getHeight()/sqSize;
+        double[][] grid = new double[xn][yn];
+        for (int i = 0; i < xn; i++) {
+            for (int j = 0; j < yn; j++) {
+                var temp = pixToCart(i * sqSize, j * sqSize);
+                grid[i][j] = expression.setVariable("x", temp[0]).setVariable("y", temp[1]).evaluate();
+            }
+        }
+        marchSquare(grid);
     }
     private static void graph(){
         double leftBound = offsetX - frame.getWidth() * SCALE;
@@ -94,9 +117,7 @@ public class Grapher {
         double widthSize = rightBound - leftBound;
         double dx = widthSize / (samples + 1);
         Graphics graphics = canvas.getGraphics();
-        clear();
-        drawAxis(bounds);
-        graphics.setColor(Color.BLACK);
+        graphics.setColor(Color.RED);
         for (int i = 0; i < samples + 2; i++) {
             double x = leftBound + (i * dx);
             double y = expression.setVariable("x",x).evaluate();
@@ -114,6 +135,16 @@ public class Grapher {
         graphics.fillRect(0, 0, frame.getWidth(), frame.getHeight());
         //graphics.setColor(temp);
     }
+    private static void regraph() {
+        clear();
+        drawAxis();
+        if (implicit) {
+            implicitGraph();
+        }
+        else {
+            graph();
+        }
+    }
     private static Point cartToPix(double leftBound, double rightBound, double downBound, double upBound, double x, double y) {
         double widthSize = rightBound - leftBound;
         double heightSize = upBound - downBound;
@@ -127,16 +158,24 @@ public class Grapher {
     private static Point cartToPix(double x, double y, double... bounds) {
         return cartToPix(bounds[0], bounds[1], bounds[2], bounds[3], x, y);
     }
-    private static double[] pixToCart(double leftBound, double rightBound, double downBound, double upBound, int x, int y) {
+    private static double[] pixToCart(int x, int y) {
+        double leftBound = offsetX - frame.getWidth() * SCALE;
+        double rightBound = offsetX + frame.getWidth() * SCALE;
+        double downBound = offsetY - frame.getHeight() * SCALE;
+        double upBound = offsetY + frame.getHeight() * SCALE;
+
         double widthSize = rightBound - leftBound;
         double heightSize = upBound - downBound;
         double xTemp = (x / (double) frame.getWidth()) * widthSize + leftBound;
         double yTemp = upBound - (y / (double) frame.getHeight()) * heightSize;
         return new double[]{xTemp, yTemp};
     }
+    private static void drawAxis() {
+        drawAxis(getBounds());
+    }
     private static void drawAxis(double... bounds) {
         Graphics graphics = canvas.getGraphics();
-        graphics.setColor(MARKER);
+        graphics.setColor(Color.BLACK);
         //draw vertical marker line
         Point center = cartToPix( 0, 0, bounds);
         if (bounds[LEFT] <= 0 && bounds[RIGHT] >= 0) {
@@ -144,6 +183,61 @@ public class Grapher {
         }
         if (bounds[DOWN] <= 0 && bounds[UP] >= 0) {
             graphics.drawLine(0, center.y, frame.getWidth(), center.y);
+            graphics.fillPolygon(new int[]{0, 10, 10}, new int[]{center.y, center.y - 3, center.y + 3}, 3);
+            graphics.fillPolygon(new int[]{frame.getWidth() - 10, frame.getWidth() - 20, frame.getWidth() - 20}, new int[]{center.y, center.y + 3, center.y - 3}, 3);
         }
+    }
+    private static void marchSquare(double[][] grid) {
+        Graphics graphics = canvas.getGraphics();
+        graphics.setColor(Color.BLUE);
+        for (int i = 0; i < grid.length - 1; i++) {
+            for (int j = 0; j < grid[i].length - 1; j++) {
+                int count = 0;
+                Point[] arr = new Point[4];
+                if ((grid[i][j] > threshold) != (grid[i + 1][j] > threshold)) {
+                    double a = grid[i][j];
+                    double b = grid[i + 1][j];
+                    double span = (a - b);
+                    double lerp = Math.abs((threshold - a) / span);
+                    arr[count] = new Point(i * sqSize, (int) Math.round(j * sqSize - lerp * sqSize));
+                    count++;
+                }
+                if ((grid[i + 1][j] > threshold) != (grid[i + 1][j + 1] > threshold)) {
+                    double a = grid[i + 1][j];
+                    double b = grid[i + 1][j + 1];
+                    double span = (a - b);
+                    double lerp = Math.abs((threshold - a) / span);
+                    arr[count] = new Point((int) Math.round(i * sqSize + lerp * sqSize), j * sqSize);
+                    count++;
+                }
+                if ((grid[i + 1][j + 1] > threshold) != (grid[i][j + 1] > threshold)) {
+                    double a = grid[i + 1][j + 1];
+                    double b = grid[i][j + 1];
+                    double span = (a - b);
+                    double lerp = Math.abs((threshold - a) / span);
+                    arr[count] = new Point(i * sqSize, (int) Math.round(j * sqSize + lerp * sqSize));
+                    count++;
+                }
+                if ((grid[i][j + 1] > threshold) != (grid[i][j] > threshold)) {
+                    double a = grid[i][j + 1];
+                    double b = grid[i][j];
+                    double span = (a - b);
+                    double lerp = Math.abs((threshold - a) / span);
+                    arr[count] = new Point((int) Math.round(i * sqSize - lerp * sqSize), j * sqSize);
+                    count++;
+                }
+                if (count == 2) {
+                    graphics.drawLine(arr[0].x, arr[0].y, arr[1].x, arr[1].y);
+                }
+            }
+        }
+    }
+    private static double[] getBounds() {
+        double leftBound = offsetX - frame.getWidth() * SCALE;
+        double rightBound = offsetX + frame.getWidth() * SCALE;
+        double downBound = offsetY - frame.getHeight() * SCALE;
+        double upBound = offsetY + frame.getHeight() * SCALE;
+
+        return new double[] {leftBound, rightBound, downBound, upBound};
     }
 }
